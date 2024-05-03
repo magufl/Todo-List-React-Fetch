2f82eff5d8f21a105eb129dc55607a4c2bc2e9b0
Mar Aguayo Florit <154342626+magufl@users.noreply.github.com>
2024-05-03 10:32:23 +0000

    con muchos errores

---
+++ src/js/component/ToDoJPH.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { LibManifestPlugin } from "webpack";
+
+
+export const ToDoJPH = () => {
+    const [todos, setTodos] = useState();
+    const host = 'https://playground.4geeks.com/todo'
+
+    const getTodos = async () => {
+        const uri = host + '/users/';
+        const options = {
+            method: 'GET'
+        };
+        const response = await fetch(uri, options);
+        if (!response.ok) {
+            //tratamos el error
+            if (response.status == '404') {
+                console.log('bad request 404')
+            }
+            console.log('Error: ', response.status, response.statusText)
+            return
+        }
+        const data = await response.json()
+        setTodos(data)
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, [])
+
+    return (
+        <div className="container text-start">
+            <h1 className="text-center">ToDo JPH</h1>
+            <>
+                {!todos ? <p>No tengo nada</p> :
+
+                    <ul className="list-group">
+
+                        {todos.map ((item) =>
+                            <li key={item.id} className="list-group-item">
+                                {item.name}
+                            </li>
+                        )}
+                    </ul>
+                }
+            </>
+
+
+        </div>
+
+    )
+}
